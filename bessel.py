320511dabe62445cff4d5e268a0fef2eca54c9cc
Michael Bella <michael.j.bella@gmail.com>
2015-02-08 16:37:09 -0800

    All the functions return dictionaries now!

--- bessel.py
+++ bessel.py
@@ -36,40 +36,49 @@ r = args.resistance
 if bw:
     fl = f - bw/2
     fh = f + bw/2
-
+else:
+    print "Bandwidth not given! No bandpass filter for you!"
+    
 coef = COEFS[order-1]
 
 # input the normalized values, output the denormalized values for L&C
 def lpl(L, R, F):
-    # Return the low pass inductor value
+    ''' Lowpass inductors
+	Return the low pass inductor value '''
     return (L * R) / (2 * math.pi * F)
 
 def lpc(C, R, F):
-    # Return the low pass cap value
+    ''' Lowpass capacitors
+	Return the low pass cap value '''
     return C / (2 * math.pi * F * R)
 
 def hpl(C, R, F):
-    # Return the high pass inductor value
+    ''' Highpass inductors
+	Return the high pass inductor value '''
     return R / (2 * math.pi * F * C)
 
 def hpc(L, R, F):
-    # Return the high pass cap value
+    ''' Highpass capacitors
+	 Return the high pass cap value '''
     return 1 / (2 * math.pi * F * R * L)
 
 def bpplc(Fh, Fl, R, L):
-    # return the parallel L & C values
+    ''' Bandpass parallel LCs 
+	Return the parallel L & C values '''
     Cp = (Fh - Fl)/(2 * math.pi * Fh * Fl * R * L)
     Lp = (R * L) / (2 * math.pi * (Fh - Fl))
     return (Cp,Lp)
 
 def bpslc(Fh, Fl, R, C):
-    # Return the series L & C values
+    ''' Bandpass series LCs
+	Return the series L & C values '''
     Cs = C / (2 * math.pi * (Fh - Fl) * R)
     Ls = ((Fh - Fl) * R) / (2 * math.pi * Fh * Fl * C)
     return (Cs,Ls)
 
 # Calculate full sequences of component values (for a set of values from the table above) for a filter.
 def lp_filter(F, R, Cn, Ln):    
+    ''' Calculate all the component values for a low pass filter '''
     parts = {}
     for i, l in enumerate(Ln):
 	ref = "L%d"%(i+1)
@@ -82,24 +91,36 @@ def lp_filter(F, R, Cn, Ln):
     return parts
 
 def hp_filter(F, R, Cn, Ln):
-    inductors = []
-    for l in Ln:
-	inductors.append(hpl(l, R, F))
-    capacitors = []
-    for c in Cn:
-	capacitors.append(hpc(c, R, F))
+    ''' Calculate all the component values for a high pass filter '''
+    parts = {}
+    for i, l in enumerate(Ln):
+	ref = "L%d"%(i+1)
+	parts[ref] = hpl(l, R, F)
+    
+    for i, c in enumerate(Cn):
+	ref = "C%d"%(i+1)
+	parts[ref] = hpc(c, R, F)
 
-    return inductors, capacitors
+    return parts
 
 def bp_filter(Fh, Fl, R, Cn, Ln):
-    parallel_LC = []
-    series_LC = []
-    for L in Ln:
-	parallel_LC.append(bpplc(Fh, Fl, R, L))
-    for C in Cn:
-	series_LC.append(bpslc(Fh, Fl, R, C))
+    ''' Calculate all the component values for a bandpass filter '''
+    parts = {}
+    for i, L in enumerate(Ln):
+	cref = "Cp%d" % ((i*2)+1)
+	lref = "Lp%d" % ((i*2)+1)
+	a, b, = bpplc(Fh, Fl, R, L)
+	parts[cref] = a
+	parts[lref] = b
 
-    return parallel_LC, series_LC
+    for i, C in enumerate(Cn):
+	cref = "Cs%d" % ((i*2)+1)
+	lref = "Ls%d" % ((i*2)+1)
+	a, b = bpslc(Fh, Fl, R, C)
+	parts[cref] = a
+	parts[lref] = b
+	
+    return parts
 
 # Run the denormalization process for high pass, low pass, or band pass.
 def denorm(cut, res, order, ftype, coeffs):
